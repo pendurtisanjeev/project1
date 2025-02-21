e8b514b349847de840549c38131eebdf410bb95c
advithraj <devops396@gmail.com>
2025-02-21 07:54:26 +0000

    create deployment script

---
+++ script.sh
@@ -0,0 +1,18 @@
+#!bin/bash
+#
+#echo "updating system"
+#sudo apt update -y
+#
+#echo "installing utilities"
+#sudo apt install -y zip unzip
+#
+#echo "install ngnix web server"
+#sudo apt install -y ngnix
+#
+#echo "remove the samples pages"
+#sudo "rm -rf /var/www/html
+#
+#echo "clone login app"
+#sudo git clone https://github.com/pendurtisanjeev/project1.git /var/www/html/
+#
+#echo "script execution completed"
